d72782c8d6550002f645f1c1714e96c1768eda77
AgungSahrulBahri <agungsahrul.as@gmail.com>
2020-04-21 22:26:58 +0700

    fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call([Mapel_StudentSeeder::class, MapelSeeder::class, StudentSeeder::class, TeacherSeeder::class, Teacher_StudentSeeder::class]);
+        $this->call([Mapel_StudentSeeder::class, MapelSeeder::class, StudentSeeder::class, TeacherSeeder::class]);
         
     }
 }
