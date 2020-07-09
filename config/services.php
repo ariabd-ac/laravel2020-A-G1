2417c48eb579939427a32dac57556df5c1978552
ariabd-ac <ariabd.com@gmail.com>
2020-07-09 13:07:35 +0700

    up

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/login/github/callback'
+        'redirect' => env('GITHUB_URL')
     ],
     
 
